6c218f16237fd52906b0a1560e9eae5213521191
Sergio <133604977+SergioMartinov31@users.noreply.github.com>
2025-11-05 13:28:29 +0300

    Delete build directory

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/sergejmartynov/Projects/OPP_lab4
-# Build directory: /Users/sergejmartynov/Projects/OPP_lab4/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(Lab4Tests "/Users/sergejmartynov/Projects/OPP_lab4/build/Lab4_tests")
-set_tests_properties(Lab4Tests PROPERTIES  _BACKTRACE_TRIPLES "/Users/sergejmartynov/Projects/OPP_lab4/CMakeLists.txt;20;add_test;/Users/sergejmartynov/Projects/OPP_lab4/CMakeLists.txt;0;")
